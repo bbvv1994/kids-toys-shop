25600442a9bcb0231f754782f81dc54a96a39226
Karl Zeiss <wexkwasexort@gmail.com>
2025-08-04 18:47:35 +0300

    Add static files serving and public folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -228,6 +228,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use('/uploads', express.static('uploads'));
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 
 app.use(passport.initialize());
 
