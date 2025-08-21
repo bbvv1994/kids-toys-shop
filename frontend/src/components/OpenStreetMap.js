9d800a0e173cd55672234f82412bc4949eafd1c6
Karl Zeiss <wexkwasexort@gmail.com>
2025-08-21 22:23:23 +0300

    Revert to standard OpenStreetMap tiles for all languages - simpler and more reliable

--- frontend/src/components/OpenStreetMap.js
+++ frontend/src/components/OpenStreetMap.js
@@ -4,6 +4,7 @@ import { Box, Typography } from '@mui/material';
 import { LocationOn } from '@mui/icons-material';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useTranslation } from 'react-i18next';
 
 // Исправляем проблему с иконками маркеров в Leaflet
 delete L.Icon.Default.prototype._getIconUrl;
@@ -14,6 +15,9 @@ L.Icon.Default.mergeOptions({
 });
 
 const OpenStreetMapComponent = ({ stores, center, zoom = 8 }) => {
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language;
+  
   const mapContainerStyle = {
     width: '100%',
     height: '400px'
@@ -24,6 +28,27 @@ const OpenStreetMapComponent = ({ stores, center, zoom = 8 }) => {
     lng: 34.9977
   };
 
+  // Выбираем тайлы в зависимости от языка
+  const getTileLayerUrl = () => {
+    // Используем стандартные OpenStreetMap тайлы для всех языков
+    return 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+  };
+
+  // Получаем атрибуцию в зависимости от языка
+  const getAttribution = () => {
+    switch (currentLanguage) {
+      case 'ru':
+        return '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> участники';
+      case 'he':
+        return '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+      default:
+        return '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    }
+  };
+
+  // Примечание: Карты используют стандартные OpenStreetMap тайлы
+  // Названия улиц отображаются на иврите (местный язык)
+
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <MapContainer
@@ -33,8 +58,9 @@ const OpenStreetMapComponent = ({ stores, center, zoom = 8 }) => {
         scrollWheelZoom={true}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution={getAttribution()}
+          url={getTileLayerUrl()}
+          maxZoom={19}
         />
         
         {stores.map((store, index) => (
